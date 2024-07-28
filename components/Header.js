62f2abdb5d31f924175d81ba6177c0585d71f459
gowthamkish <gowthamkish@outlook.com>
2024-07-28 14:45:03 +0530

    Added favicon and Logo image

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,22 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import styles from "@/css/Header.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/images/logo.jpeg";
 const Header = () => {
   return (
-    <Navbar className={styles.header} bg="dark" data-bs-theme="dark">
+    <Navbar className={styles.header} expand="lg" bg="dark" data-bs-theme="dark">
       <Container fluid>
-        <Navbar.Brand href="/">Navbar</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <Image width={40} height={40} src={logo} rounded />
+        </Navbar.Brand>
         <Nav className="justify-content-end">
-          <Link className="text-white p-2" href="/auth/login">Login</Link>
-          <Link className="text-white p-2" href="/auth/register">Register</Link>
+          <Link className="text-white p-2" href="/auth/login">
+            Login
+          </Link>
+          <Link className="text-white p-2" href="/auth/register">
+            Register
+          </Link>
         </Nav>
       </Container>
     </Navbar>
